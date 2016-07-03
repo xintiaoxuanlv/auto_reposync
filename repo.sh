48a5bb5484d849a55584e88b33c0c8bad906114f
Melody <843243689@qq.com>
2016-07-03 10:21:27 +0800

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -3,6 +3,6 @@ echo "======start repo sync======"
 repo sync -j32 -f --no-clone-bundle 
 while [ $? == 1 ]; do  
 echo "======sync failed, re-sync again======"  
-sleep 3  
+sleep 1  
 repo sync -j32 -f --no-clone-bundle
 done
